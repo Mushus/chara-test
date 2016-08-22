01f22f553ed2530bc0de0f6605aebeb2d7f1376d
Mushus <mushus.wynd@gmail.com>
2016-08-23 01:05:52 +0900

    リファクタリング

--- script/game.js
+++ script/game.js
@@ -1,68 +1,89 @@
-"use strict";
+!function(global) {
+  "use strict";
 
-var Chara = function(mesh) {
-  var self = this;
-
-  this.mesh = mesh;
-  this.emotion = new Emotion(mesh.material);
-  this.mixer = new THREE.AnimationMixer(mesh);
-  this.state = new CharaState(this);
-  this._ready = null;
-  this.move = null;
-  var materials = mesh.material.materials;
-  for (var i in materials) {
-    materials[i].skinning = true;
+  /** キャラクタ */
+  global.Chara = function Chara(mesh) {
+    this.mesh = mesh;
+    this.emotion = new Emotion(mesh.material);
+    this.mixer = new THREE.AnimationMixer(mesh);
+    this.state = new CharaState(this);
+    this.ready = null;
+    this.move = null;
+    var materials = mesh.material.materials;
+    for (var i in materials) {
+      materials[i].skinning = true;
+    }
+    console.log(this);
+    this.watchImages();
   }
-  this.watchImages();
-}
 
-Chara.prototype = {
-  moveChange: function(actionName) {
+  global.Chara.prototype = Object.create(new Object(), {
+    moveChange: { value: moveChange },
+    moveClossfade: { value: moveCrossfade },
+    watchImages: { value: watchImages },
+    update: { value: update }
+  });
+
+  /** モーション変更 */
+  function moveChange(actionName) {
     var animations = this.mesh.geometry.animations;
     for (var i in animations) {
       this.mixer.clipAction(animations[i]).setEffectiveWeight(1).stop();
     }
     this.mixer.clipAction(actionName).setEffectiveWeight(1).play(0);
     this.move = actionName;
-  },
-  moveClossfade: function(toActionName, duration) {
+  }
+
+  /** モーションをなめらかに接続する */
+  // TODO: durationがモーションの長さに依存するのでどうにか秒に変更する
+  function moveCrossfade(toActionName, duration) {
     var animations = this.mesh.geometry.animations;
-    var fromAction = this.mixer.clipAction(this.move)
+    var fromAction = this.mixer.clipAction(this.move);
     var toAction = this.mixer.clipAction(toActionName);
     toAction.play(0);
     fromAction.crossFadeTo(toAction, duration);
-
     this.move = toActionName;
-  },
-  ready: function(func) {
-    this._ready = func;
-  },
-  watchImages: function() {
-    var self = this;
+  }
+
+  /** テクスチャの画像を更新する */
+  function watchImages() {
+    var _this = this;
     var materials = this.mesh.material.materials;
+    var image;
     for (var i in materials) {
       var image = materials[i].map.image;
+      // HACK: 読み込まれたら image が設定される
       if (image == null) {
-        setTimeout(function() { self.watchImages(); });
+        // TODO: 理想はTextureLoaderの中身監視だけど…あったら直す
+        setTimeout(function() { _this.watchImages(); });
         return;
       }
     }
-    if (self._ready) self._ready.call(self);
-  },
-  update: function(delta) {
+    if (_this.ready) _this.ready.call(_this);
+  }
+
+  /** キャラの動きを更新する */
+  function update(delta) {
     this.state.update(delta);
     this.mixer.update(delta);
   }
-};
 
-var Emotion = function(material) {
-  this.material = material;
-  this.emotions = [];
-}
+}(this);
 
-Emotion.prototype = {
-  /** 表情情報登録 */
-  register: function(name, offsetX, offsetY) {
+!function(global) {
+  /** テクスチャのオフセットをずらす方式で表情を変える */
+  global.Emotion = function(material) {
+    this.material = material;
+    this.emotions = [];
+  }
+
+  global.Emotion.prototype = Object.create(new Object(), {
+    register: { value: register },
+    change: { value: change }
+  });
+
+  /** 表情を登録する */
+  function register(name, offsetX, offsetY) {
     var materials = this.material.materials;
     for (var i in materials) {
       if (materials[i].name == name) {
@@ -73,10 +94,10 @@ Emotion.prototype = {
         };
       }
     }
-  },
+  }
 
   /** 表情を変える */
-  change: function(name, num) {
+  function change(name, num) {
     var emotionInfo = this.emotions[name];
     var posX = 0, posY = 0;
     for (var i = 0; i < num; i ++) {
@@ -89,32 +110,47 @@ Emotion.prototype = {
     emotionInfo.texture.offset.x = posX;
     emotionInfo.texture.offset.y = posY;
   }
-};
-var CharaState = function(target) {
-  this.state = null;
-  this.states = [];
-  this.target = target;
-  this.timers = [];
-  this.elapsedTime = 0;
-}
 
-CharaState.prototype = {
-  register: function(name, func) {
+}(this);
+
+!function(global) {
+  /** キャラの状態 */
+  global.CharaState = function(target) {
+    this.state = null;
+    this.states = [];
+    this.target = target;
+    this.timers = [];
+    this.elapsedTime = 0;
+  }
+
+  global.CharaState.prototype = Object.create(new Object(), {
+    register: { value: register },
+    change: { value: change },
+    timer: { value: timer },
+    update: { value: update }
+  });
+
+  /** ステートを登録 */
+  function register(name, func) {
     this.states[name] = {
       func: func
     };
-  },
-  change: function(name) {
+  }
+
+  /** ステートを変更する */
+  function change(name) {
     this.timers = [];
     this.elapsedTime = 0;
 
     this.state = this.states[name];
     this.state.func.call(this, this.target);
-  },
-  timer: function(time, func) {
+  }
+
+  /** タイマーを設定する */
+  function timer(time, func) {
     var setTime = time + this.elapsedTime;
 
-    // 小さい順に入れる
+    // 小さい順に入れることで取り出すときに楽になる
     var idx;
     for (idx = 0; idx < this.timers.length; idx++) {
       if (this.timers[idx].time > setTime) {
@@ -125,149 +161,91 @@ CharaState.prototype = {
       time: setTime,
       func: func
     });
-  },
-  update: function(delta) {
+  }
+
+  /** ステートを更新する */
+  function update(delta) {
     var state = this.state;
     this.elapsedTime += delta;
 
+    // タイマー実行するためにタイマーの一覧を確認
     for (var i = 0; i < this.timers.length; i++) {
       if (this.timers[i].time > this.elapsedTime) {
         break;
       } else {
         this.timers[i].func.call(this, this.target);
         if (this.state != state) {
+          // 状態が変わってたら他のはすべて取りやめ
           break;
         } else {
+          // 一つ削除
           this.timers.shift();
           i--;
         }
       }
     }
   }
-};
 
-var Game = function(elem, modelPath) {
-  var self = this;
-  this.elem = $(elem);
-  // キャラクタ
-  this.chara = null;
-  // 画面
-  this.screen = {
-    width: 0,
-    height: 0
-  };
+}(this);
 
-  this.clock = new THREE.Clock();
+!function(global) {
+  global.Game = function(elem, modelPath) {
+    this.elem = $(elem);
+    // 画面
+    this.screen = {
+      width: 0,
+      height: 0
+    };
 
-  // シーン
-  this.scene = new THREE.Scene();
+    this.clock = new THREE.Clock();
 
-  // カメラ
-  this.camera = new THREE.PerspectiveCamera(45, 1, 1, 1000);
-  this.camera.position.set(0, .8, 3.5);
-  this.camera.lookAt(new THREE.Vector3(0, .8, 0));
-  this.scene.add(this.camera);
+    // シーン
+    this.scene = new THREE.Scene();
 
-  // ライト
-  var directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);　　
-  directionalLight.position.set(0, 60, -200);　　
-  var ambientLight = new THREE.AmbientLight(0xffffff);　　
-  this.scene.add(directionalLight, ambientLight);
+    // カメラ
+    this.camera = new THREE.PerspectiveCamera(45, 1, 1, 1000);
+    this.camera.position.set(0, .8, 3.5);
+    this.camera.lookAt(new THREE.Vector3(0, .8, 0));
+    this.scene.add(this.camera);
 
-  // レンダラー
-  this.renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    alpha: true
+    // ライト
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);　　
+    directionalLight.position.set(0, 60, -200);　　
+    var ambientLight = new THREE.AmbientLight(0xffffff);　　
+    this.scene.add(directionalLight, ambientLight);
+
+    // レンダラー
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true
+    });
+    this.renderer.setClearColor(0xffffff, 0);
+    this.renderer.setPixelRatio(window.devicePixelRatio? window.devicePixelRatio : 1);
+    this.elem.append(this.renderer.domElement);
+
+    this.updateSize();
+    this.rendering();
+  }
+
+  Game.prototype = Object.create(new Object(), {
+    rendering: { value: rendering },
+    render: { value: render },
+    updateSize: { value: updateSize }
   });
-  this.renderer.setClearColor(0xffffff, 0);
-  this.renderer.setPixelRatio(window.devicePixelRatio? window.devicePixelRatio : 1);
-  this.elem.append(this.renderer.domElement);
 
-  // モデルローダー
-  var loader = new THREE.JSONLoader();
-  loader.load(modelPath, function(get, mat) {
-    self.chara = self.loadChara(get, mat);
-  });
-
-  this.updateSize();
-  this.rendering();
-}
-
-Game.prototype = {
-  // レンダリングループ
-  rendering: function() {
-    var delta = this.clock.getDelta();
-    if (this.chara) {
-      this.chara.update(delta);
-    }
+  function rendering() {
+    if (this.render) this.render();
     this.renderer.render(this.scene, this.camera);
 
-    var self = this;
-    requestAnimationFrame(function() { self.rendering(); }, this.renderer.domElement);
-  },
-  // ロードする
-  loadChara: function(geo, mat) {
-    var material = new THREE.MultiMaterial(mat);
-    var mesh = new THREE.SkinnedMesh(geo, material);
-    this.scene.add(mesh);
+    var _this = this;
+    requestAnimationFrame(function() { _this.rendering(); }, this.renderer.domElement);
+  }
 
-    var chara = new Chara(mesh);
+  function render() {
+    // empty
+  }
 
-    chara.emotion.register('mouth', 120 / 512, 80 / 512);
-    chara.emotion.register('eye', 100 / 512, 100 / 512);
-    chara.emotion.register('cheek', 160 / 512, 160 / 512);
-
-    chara.state.register('idle', function(chara) {
-      if (chara.move != 'idle') {
-        console.log(chara.move)
-        chara.moveChange('idle');
-      }
-    });
-    chara.state.register('shake_hand', function(chara) {
-      chara.moveChange('shake_hand');
-      this.timer(25 / 24, function() {
-        chara.moveClossfade('idle', 8/33);
-      });
-      this.timer(33 / 24, function() {
-        this.change('idle');
-      });
-    });
-    chara.state.register('nono', function(chara) {
-      chara.moveChange('nono');
-      this.timer(42 / 24, function() {
-        chara.moveClossfade('idle', 8/50);
-      });
-      this.timer(50 / 24, function() {
-        this.change('idle');
-      });
-    });
-    chara.state.register('yeah', function(chara) {
-      chara.moveChange('yeah');
-      this.timer(36 / 24, function() {
-        chara.moveClossfade('idle', 8/44);
-      });
-      this.timer(44 / 24, function() {
-        this.change('idle');
-      });
-    });
-    chara.state.register('huh', function(chara) {
-      chara.moveChange('huh');
-      this.timer(40 / 24, function() {
-        chara.moveClossfade('idle', 15/55);
-      });
-      this.timer(55 / 24, function() {
-        this.change('idle');
-      });
-    });
-
-    chara.ready(function() {
-      this.state.change('shake_hand');
-    });
-
-    return chara;
-  },
-
-  updateSize: function() {
+  function updateSize() {
     this.screen.width = this.elem.width();
     this.screen.height = this.elem.height();
 
@@ -276,57 +254,64 @@ Game.prototype = {
 
     this.renderer.setSize(this.screen.width, this.screen.height);
   }
-};
 
-var DragController = function(elem, camera, lookAt) {
-  this.camera = camera;
-  this.lookAt = lookAt;
-  this.speed = 0.3;
+}(this);
 
-  this.dragX = 0;
-  this.dragY = 0;
-  this.cameraLength = 3.5;
-  this.pitch = 0;
-  this.yaw = 0;
+!function(global) {
+  global.DragController = function(elem, camera, lookAt) {
+    this.camera = camera;
+    this.lookAt = lookAt;
+    this.speed = 0.3;
 
-  var self = this;
+    this.dragX = 0;
+    this.dragY = 0;
+    this.cameraLength = 3.5;
+    this.pitch = 0;
+    this.yaw = 0;
 
-  this.mouseDown = false;
-  $(elem).on('mousedown', function(e) {
-    self.mouseDown = true;
-    self.dragStart(e.pageX, e.pageY);
-    e.preventDefault();
-  });
-  $(document).on('mouseup', function(e) {
-    self.mouseDown = false;
-    e.preventDefault();
-  });
-  $(document).on('mousemove', function(e) {
-    if (self.mouseDown) {
-      self.dragMove(e.pageX, e.pageY);
+    var self = this;
+
+    this.mouseDown = false;
+    $(elem).on('mousedown', function(e) {
+      self.mouseDown = true;
+      self.dragStart(e.pageX, e.pageY);
       e.preventDefault();
-    }
-  });
-  $(elem).on('touchstart', function(e) {
-    var touch = e.targetTouches[0];
-    self.dragStart(touch.pageX, touch.pageY);
-    e.preventDefault();
-  });
-  $(elem).on('touchmove', function(e) {
-    if (e.targetTouches.length == 1) {
+    });
+    $(document).on('mouseup', function(e) {
+      self.mouseDown = false;
+      e.preventDefault();
+    });
+    $(document).on('mousemove', function(e) {
+      if (self.mouseDown) {
+        self.dragMove(e.pageX, e.pageY);
+        e.preventDefault();
+      }
+    });
+    $(elem).on('touchstart', function(e) {
       var touch = e.targetTouches[0];
-      self.dragMove(touch.pageX, touch.pageY);
+      self.dragStart(touch.pageX, touch.pageY);
       e.preventDefault();
-    }
-  });
-};
+    });
+    $(elem).on('touchmove', function(e) {
+      if (e.targetTouches.length == 1) {
+        var touch = e.targetTouches[0];
+        self.dragMove(touch.pageX, touch.pageY);
+        e.preventDefault();
+      }
+    });
+  };
 
-DragController.prototype = {
-  dragStart: function(x, y) {
+  global.DragController.prototype = Object.create(new Object(), {
+    dragStart: { value: dragStart },
+    dragMove: { value: dragMove }
+  });
+
+  function dragStart(x, y) {
     this.dragX = x;
     this.dragY = y;
-  },
-  dragMove: function(x, y) {
+  }
+
+  function dragMove(x, y) {
     var deltaX = (x - this.dragX) * this.speed;
     var deltaY = (y - this.dragY) * this.speed;
     this.dragX = x;
@@ -343,4 +328,4 @@ DragController.prototype = {
     this.camera.position.z = this.lookAt.z + Math.cos(this.pitch) * Math.cos(this.yaw)* this.cameraLength;
     this.camera.lookAt(this.lookAt);
   }
-};
+}(this);
---
+++ script/main.js
@@ -0,0 +1,92 @@
+!function(global) {
+  "use strict";
+
+  global.Main = function(elem, modelPath) {
+    Game.call(this, elem, modelPath);
+
+    var _this = this;
+    // モデルローダー
+    var loader = new THREE.JSONLoader();
+    loader.load(modelPath, function(geo, mat) {
+      var material = new THREE.MultiMaterial(mat);
+      var mesh = new THREE.SkinnedMesh(geo, material);
+      _this.scene.add(mesh);
+
+      _this.chara = _this.loadChara(mesh);
+    });
+  }
+
+  global.Main.prototype = Object.create(Game.prototype, {
+    loadChara: { value: loadChara },
+    render: { value: render }
+  });
+
+  /** キャラの読み込み */
+  function loadChara(mesh) {
+      var chara = new Chara(mesh);
+
+      // エモーション
+      chara.emotion.register('mouth', 120 / 512, 80 / 512);
+      chara.emotion.register('eye', 100 / 512, 100 / 512);
+      chara.emotion.register('cheek', 160 / 512, 160 / 512);
+
+      // モーションをどう動かすかとか
+      chara.state.register('idle', function(chara) {
+        if (chara.move != 'idle') {
+          chara.moveChange('idle');
+        }
+      });
+      chara.state.register('shake_hand', function(chara) {
+        chara.moveChange('shake_hand');
+        this.timer(25 / 24, function() {
+          chara.moveClossfade('idle', 8/33);
+        });
+        this.timer(33 / 24, function() {
+          this.change('idle');
+        });
+      });
+      chara.state.register('nono', function(chara) {
+        chara.moveChange('nono');
+        this.timer(42 / 24, function() {
+          chara.moveClossfade('idle', 8/50);
+        });
+        this.timer(50 / 24, function() {
+          this.change('idle');
+        });
+      });
+      chara.state.register('yeah', function(chara) {
+        chara.moveChange('yeah');
+        this.timer(36 / 24, function() {
+          chara.moveClossfade('idle', 8/44);
+        });
+        this.timer(44 / 24, function() {
+          this.change('idle');
+        });
+      });
+      chara.state.register('huh', function(chara) {
+        chara.moveChange('huh');
+        this.timer(40 / 24, function() {
+          chara.moveClossfade('idle', 15/55);
+        });
+        this.timer(55 / 24, function() {
+          this.change('idle');
+        });
+      });
+
+      // 読み込み終わったら手を振る
+      chara.ready = function() {
+        this.state.change('shake_hand');
+      };
+
+      return chara;
+  }
+
+  // レンダリングする
+  function render() {
+    var delta = this.clock.getDelta();
+    if (this.chara) {
+      this.chara.update(delta);
+    }
+  }
+
+}(this);
